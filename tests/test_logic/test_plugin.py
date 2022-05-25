0ea4b9160b303ad762386b6b198c2d428eaa04d5
Vladimir Kurilin <kurilin@tochka.com>
2022-05-25 14:20:02 +0700

    test(exceptions-config): tests for exception ordering

--- tests/test_logic/test_plugin.py
+++ tests/test_logic/test_plugin.py
@@ -2,6 +2,8 @@
 from pathlib import Path
 
 # project
+import pytest
+
 from flakeheaven._logic import get_exceptions
 
 
@@ -46,3 +48,47 @@ def test_get_exceptions_with_intersections(tmp_path: Path):
 
     result = get_exceptions(path=test_file_path, exceptions=exceptions, root=tmp_path)
     assert result == {'pyflakes': ['+*'], 'pycodestyle': ['+*']}
+
+
+@pytest.mark.parametrize(
+    'first_match,better_match',
+    (
+        ('root/dir_1', 'root/dir_1/dir_1_1'),
+        ('*/dir1/*', '*/dir_1/dir_1_1/*'),
+        ('*/dir1/*', 'root/dir_1/dir_1_1'),
+    ),
+)
+def test_exceptions_priority(tmp_path: Path, first_match, better_match):
+    exceptions = {
+        first_match: {'plugin_name': ['+FIRST_MATCH_RULE']},
+        better_match: {'plugin_name': ['+BETTER_MATCH_RULE']},
+    }
+
+    test_file_path = _setup_filetree_for_priority_test(tmp_path)
+    result = get_exceptions(path=test_file_path, exceptions=exceptions, root=tmp_path)
+
+    assert result == {'plugin_name': ['+BETTER_MATCH_RULE']}
+
+
+def _setup_filetree_for_priority_test(tmp_path: Path) -> Path:
+    """
+    root
+    └─ dir_1
+       └─ dir_1_1
+          └─ file_1.py
+
+    Returns: path for file_1.py
+    """
+    root_dir = tmp_path / 'root'
+    root_dir.mkdir()
+
+    dir_1 = root_dir / 'dir_1'
+    dir_1.mkdir()
+
+    dir_1_1 = dir_1 / 'dir_1_1'
+    dir_1_1.mkdir()
+
+    file_1_path = dir_1_1 / 'file_1.py'
+    file_1_path.touch()
+
+    return file_1_path
